9c29d1d6cd4598958d09cb3a72f65bfc48095abe
itsharsh <hkumarsingh142@gmail.com>
2018-11-05 03:02:02 +0530

    login route added

--- api/apiController.js
+++ api/apiController.js
@@ -1,5 +1,6 @@
 var bodyparser = require('body-parser');
 var appliance = require('../models/applianceModel.js');
+var user = require('../models/userModel.js');
 
 module.exports = function(app){    
     app.use(bodyparser.json());
@@ -35,4 +36,32 @@ module.exports = function(app){
     app.delete('/api/:id', function(req, res){
         res.send("DELETE Request");
     });
+
+    app.post('/login', function(req, res){
+        user.find({username: req.body.username}, function(err, ack){
+            if(err) throw err;
+            if(ack[0]){
+                if(req.body.password==ack[0].password){
+                    console.log("switch");
+                    res.writeHead(301, {Location: '/htmlPages/switch.html'});
+                }
+                else{
+                    console.log("wrong password");
+                    res.writeHead(301, {Location: '/htmlPages/login.html'});
+                }
+            }
+            else{
+                console.log("invalid user");
+                res.writeHead(301, {Location: '/htmlPages/login.html'});
+            }
+        });
+    });
+
+    app.post('/register', function(req, res){
+        user.create(req.body, function(err, ack){
+            if(err) throw err;
+            res.send(ack);
+        });
+    });
+
 }
--- models/applianceModel.js
+++ models/applianceModel.js
@@ -1,11 +1,5 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
-// var userSchema = new schema({
-//     email: String,
-//     username: String,
-//     password: String,
-//     appliance: [applianceSchema]
-// });
 
 var applianceSchema = new schema({
     rid: Object,
